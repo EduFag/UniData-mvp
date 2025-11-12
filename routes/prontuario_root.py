1bbd6ac38aec2da3a074a24175d1d5c5b8897705
Eduardo Fagundes da Silva <edufagundes2005@gmail.com>
2025-11-12 18:49:58 -0300

    alteração da função de registrar paciente

--- routes/prontuario_root.py
+++ routes/prontuario_root.py
@@ -8,20 +8,15 @@ def root():
     return {"mensagem": "Bem vindo"}
 
 @router.post("/registrar")
-def registrar_paciente():
-    try:
-        tx = contrato.functions.registrarPaciente().build_transaction({
-            "from": api_signer.address,
-            "nonce": web3.eth.get_transaction_count(api_signer.address),
-            "gas": 300000,
-            "gasPrice": web3.to_wei("5", "gwei"),
-        })
+def registrar_paciente(endereco_paciente):
+    tx = contrato.functions.registrarPaciente(endereco_paciente).build_transaction({
+        "from": api_signer.address,
+        "nonce": web3.eth.get_transaction_count(api_signer.address),
+        "gas": 300000,
+        "gasPrice": web3.to_wei("5", "gwei"),
+    })
 
-        signed_tx = api_signer.sign_transaction(tx)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        return {"tx_hash": tx_hash.hex()}
-
-    except Exception as e:
-        return {"erro": str(e)}
-
+    signed_tx = api_signer.sign_transaction(tx)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    return {"tx_hash": tx_hash.hex()}
 
