2a30d0c08aff2736207ba0e59a1d90f738732fac
Eduardo Fagundes da Silva <edufagundes2005@gmail.com>
2025-11-23 00:43:32 -0300

    atualizando configuracoes

--- API/config/__init__.py
+++ API/config/__init__.py
@@ -2,11 +2,15 @@
 from .provider import get_provider
 from .signer import get_signer
 from .contract_loader import load_contract
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # carrega as variáveis do .env
 
 # Configurações globais
 web3 = get_provider()
-private_key = "CHAVE_PRIVADA_DO_ANVIL"
+private_key = os.getenv("ANVIL_PRIVATE_KEY")
 api_signer = get_signer(private_key)
 
 # Contrato principal
-contrato = load_contract("Prontuario")
+contrato = load_contract("Unidata")
